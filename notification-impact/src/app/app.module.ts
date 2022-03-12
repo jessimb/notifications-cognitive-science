950e1cd5e0425e7287ca50b4bf9961c432c033aa
jessimb <jessimb@umich.edu>
2022-03-12 13:52:47 -0500

    stash

--- notification-impact/src/app/app.module.ts
+++ notification-impact/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,10 @@ import { NotificationPanelComponent } from './notification-panel/notification-pa
     ],
     imports: [
         BrowserModule,
-        AppRoutingModule
+        AppRoutingModule,
+        // FormsModule,
+        // ReactiveFormsModule,
+        MatRadioModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
--- notification-impact/src/app/notification-panel/notification-panel.component.ts
+++ notification-impact/src/app/notification-panel/notification-panel.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-notification-panel',
-  templateUrl: './notification-panel.component.html',
-  styleUrls: ['./notification-panel.component.scss']
+    selector: 'app-notification-panel',
+    templateUrl: './notification-panel.component.html',
+    styleUrls: ['./notification-panel.component.scss']
 })
-export class NotificationPanelComponent implements OnInit {
+export class NotificationPanelComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    notifTypeOptions = [{ name: "Social Media", val: 'social' },
+    { name: "Phone call", val: 'call' },
+    { name: "Text Message", val: 'text' }];
+    notificationType = '';
+
+    constructor() { }
+
+    submit() {
+        console.log(this.notificationType);
+    }
 
 }

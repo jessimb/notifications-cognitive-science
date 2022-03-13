3d7c03b88d344d9a494a872296b568d929f0fe54
jessimb <jessimb@umich.edu>
2022-03-12 20:46:20 -0500

    setting up firebase hosting

--- notification-impact/README.md
+++ notification-impact/README.md
@@ -25,3 +25,19 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+
+## Deploy
+
+### Pre-req
+Install firebase cli: `npm install -g firebase-tools`
+
+
+### Push live
+
+Ensure you have built the app: `ng build`.
+
+Run `firebase deploy` to deploy from the current directory. 
+
+Visit https://emotifications.web.app/ to verify it was deployed.
+
--- notification-impact/src/app/app.component.ts
+++ notification-impact/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDwGEfo2yN-jH3gHTc6-QGPugjMc2eohJU",
+    authDomain: "emotifications.firebaseapp.com",
+    projectId: "emotifications",
+    storageBucket: "emotifications.appspot.com",
+    messagingSenderId: "947181223723",
+    appId: "1:947181223723:web:9e9655bb679cec3edbcf6e",
+    measurementId: "G-BZE8G3FSNG"
+};
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Notifications + Cognitive Science';
+    title = 'Notifications + Cognitive Science';
+
+    // Initialize Firebase
+    // app = initializeApp(firebaseConfig);
+    // analytics = getAnalytics(this.app);
 }

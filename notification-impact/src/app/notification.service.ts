f91a05d24c628541b78e43159c212aef9d3ff902
Jessi Krupsky <jessimb@umich.edu>
2022-03-22 20:45:08 -0400

    create blank notification service

---
+++ notification-impact/src/app/notification.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * Notification service handles the business logic for the app.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor() { }
+}

564dafb24cc688405d1aa41b337f0427c36f5977
Jessi Krupsky <jessimb@umich.edu>
2022-03-23 22:02:07 -0400

    adding a decay for the bars

--- notification-impact/src/app/cognitive-load-bar/cognitive-load-bar.component.ts
+++ notification-impact/src/app/cognitive-load-bar/cognitive-load-bar.component.ts
@@ -9,11 +9,21 @@ import { NotificationService } from '../notification.service';
 export class CognitiveLoadBarComponent {
 
   cognitiveLoadLevel = 0;
+  decayAmount = .5;
+  decayFrequencyMs = 5000;
 
   constructor(private notificationService: NotificationService) {
     notificationService.cognitiveLoad$.subscribe(
       (cognitiveLoad: number) => {
         this.cognitiveLoadLevel += cognitiveLoad;
       });
+
+      setInterval(() => {
+        this.cognitiveLoadLevel -= this.decayAmount;
+        this.cognitiveLoadLevel = Math.round(this.cognitiveLoadLevel * 100) / 100;
+        if (this.cognitiveLoadLevel < 0) {
+          this.cognitiveLoadLevel = 0;
+        }
+      }, this.decayFrequencyMs);
    }
 }
--- notification-impact/src/app/stress-bar/stress-bar.component.ts
+++ notification-impact/src/app/stress-bar/stress-bar.component.ts
@@ -9,11 +9,21 @@ import { NotificationService } from '../notification.service';
 export class StressBarComponent {
 
   stressLevel = 0;
+  decayAmount = .1;
+  decayFrequencyMs = 3000;
 
   constructor(private notificationService: NotificationService) {
     notificationService.stress$.subscribe(
       (stress: number) => {
         this.stressLevel += stress;
       });
+
+      setInterval(() => {
+        this.stressLevel -= this.decayAmount;
+        this.stressLevel = Math.round(this.stressLevel * 100) / 100;
+        if (this.stressLevel < 0) {
+          this.stressLevel = 0;
+        }
+      }, this.decayFrequencyMs);
    }
 }

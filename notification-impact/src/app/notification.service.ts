21f9e228a9a8a2035bad12ac8ff010f6a7beb300
yvtt <yvtt@users.noreply.github.com>
2022-04-06 22:57:17 -0700

    updated calculation of stress

--- notification-impact/src/app/notification.service.ts
+++ notification-impact/src/app/notification.service.ts
@@ -8,14 +8,6 @@ export interface NotificationConfigEntry {
   frequency: number,  // Send notificaton every X minutes
 }
 
-interface Emotification {
-    type: string;
-    frequency: number,
-    receiveTime: number,
-    age: number,
-    ttl: number,
-}
-
 // Keep up to date with notification-panel
 export const notificationTypes = ['Social media', 'School or work', 'Entertainment', 'Productivity'];
 
@@ -33,12 +25,6 @@ export class NotificationService {
     // Observable streams
     stressModel$ = this.modelOutput.asObservable();
 
-    // Notifications generated from settings
-    notifications: Emotification[] = [];
-
-    // Stress
-    stressArr: Number[] = [];
-
     constructor() { }
 
     updateChart(model: number) {
@@ -49,30 +35,15 @@ export class NotificationService {
         // TODO calculate model output to send to chart
         console.log(notifConfig);
 
-        // Array(1)
-        //     0: 
-        //         count: 5
-        //         frequency: 30
-        //         startTime: ""
-        //         type: "School or work"
-        
-        console.log(notifConfig[0]);
-
-        // Generate notifications
-        // for each notif config entry:
-        // make notification at start time (notification #1.receiveTime = start time)
-        // add time + frequency to time
-        // make y count notifs
-
-        // Clear notifications array
-        this.notifications.length = 0;
+        var notifications = [];
+        var stressArr = [];
 
         // Generate notifications
         for (var i  = 0; i < notifConfig.length; i++) {
             const startTimeNum = this.getTimeNum(notifConfig[i].startTime);
 
+            // Create 'count' notifications per notif config entry
             for (var j = 0; j < notifConfig[i].count; j++) {
-                console.log("Creating notification " + i + " of " + notifConfig.length);
                 const rtime = startTimeNum + (j * notifConfig[i].count)
                 const entry = {
                     type: notifConfig[i].type,
@@ -81,7 +52,7 @@ export class NotificationService {
                     age: 0,
                     ttl: this.getTTL(rtime),
                 }
-                this.notifications.push(entry);
+                notifications.push(entry);
             }
         }
 
@@ -91,66 +62,47 @@ export class NotificationService {
             var totalStress = 0;
 
             // Get stress for each notification at currTime
-            for (var n = 0; n < this.notifications.length; n++) {
-                if (this.notifications[n].receiveTime <= currTime) {
+            for (var n = 0; n < notifications.length; n++) {
+                if (notifications[n].receiveTime <= currTime) {
+                    notifications[n].age++;
                     // Remove notification if expired
-                    if (this.notifications[n].age == this.notifications[n].ttl) {
-                        this.notifications.splice(n, 1);
+                    if (notifications[n].age == notifications[n].ttl) {
+                        notifications.splice(n, 1);
+                        continue;
+                    } else {
+                        totalStress += this.getStress(notifications[n]);
                     }
-                    totalStress += this.getStress(this.notifications[n]);
                 }
             }
 
-            // Add stress at currTime to output array
-            this.stressArr.push(totalStress);
-
-            // TODO Add stress to series:
-            // modelData = [
-            //     {
-            //         "name": "Stress",
-            //         "series": [
-            //             {
-            //                 "name": new Date(2021, 4, 1, 0, 0, 0),
-            //                 "value": 0,
-            //             },
-            //             {
-            //                 "name": new Date(2021, 4, 1, 9, 0, 0),
-            //                 "value": 15,
-            //                 "tooltipText": 'work text'
-            //             },
-            //         ]
-            //     },
-            // ]
+            // Add totalStress at currTime to output array
+            const entry = {
+                name: this.getDate(currTime),
+                value: totalStress,
+            };
+            stressArr.push(entry);
         }
 
-        // Print stress values
-        console.log(this.stressArr);
+        var model = {
+            name: "Stress",
+            series: stressArr,
+        };
 
-        // 
+        console.log(model);
+        // TODO Send to graph
+    }
+
+    // Convert time represented as 0-1440 number to Date object
+    getDate(timeNum: number){
+        var hours = Math.floor(timeNum / 60);
+        var min = timeNum % 60;
+        // console.log("Time:", hours, min)
+
+        return new Date(2022, 4, 1, hours, min);
     }
 
     // Calculate stress
-    getStress(notification: Emotification) {
-        // Stress due to notificaton type
-        const stressTypeSocialMedia = 0;
-        const stressTypeSchoolWork = 0;
-        const stressTypeEntertainment = 0;
-        const stressTypeProductivity = 0;
-
-        // Stress due to notification context
-        const stressContextWorkSchool = 0;
-        const stressContextVehicle = 0;
-        const stressContextCaretaking = 0;
-        const stressContextLeisure = 0;
-        const stressContextFitness = 0;
-
-        // Stress due to notification frequency
-        const stressFreq1min = 0;
-        const stressFreq5min = 0;
-        const stressFreq15min = 0;
-        const stressFreq30min = 0;
-        const stressFreq1hour = 0;
-
+    getStress(notification: any) {
         var stressType = 0;
         var stressContext = 0;
         var stressFreq = 0;
@@ -158,45 +110,50 @@ export class NotificationService {
         // Get stress due to notification type
         switch (notification.type) {
             case 'Social media':
-                stressType = 0;
+                stressType = 3.34;
                 break;
             case 'School or work':
-                stressType = 0;
+                stressType = 3.67;
                 break;
             case 'Entertainment':
-                stressType = 0;
+                stressType = 3.23;
                 break;
             case 'Productivity':
-                stressType = 0;
+                stressType = 3.52;
                 break;
             default:
                 stressType = 0;
         }
 
         // Get stress due to notification context
+        // Currently have 9am-5pm as work, everything else as leisure
         if (notification.receiveTime >= this.getTimeNum('9:00') && notification.receiveTime <= this.getTimeNum("17:00")) {
-            stressContext = 0;
+            stressContext = 3.64;
+        } else {
+            stressContext = 3.30;
         }
 
         // Get stress due to notification frequency
         if (notification.frequency >= 1 && notification.frequency < 5) {
-            stressFreq = 0;
+            stressFreq = 3.73;
         } else if (notification.frequency >= 5 && notification.frequency < 15) {
-            stressFreq = 0;
+            stressFreq = 3.58;
         } else if (notification.frequency >= 15 && notification.frequency < 30) {
-            stressFreq = 0;
+            stressFreq = 3.43;
         } else if (notification.frequency >= 30 && notification.frequency < 60) {
-            stressFreq = 0;
+            stressFreq = 3.48;
         } else if (notification.frequency >= 60) {
-            stressFreq = 0;
+            stressFreq = 3.31;
         }
 
+        // Calculate stress factoring in TTL for decay
         var stress = stressType * stressContext * stressFreq
+        stress *= (notification.ttl - notification.age / notification.ttl);
         return stress;
     }
 
     // Get TTL based on receive time of notification
-    // Currently 9am-5pm is work/school, else is leisure
+    // Currently 9am-5pm is work/school, everything else is leisure
     getTTL(receiveTime: number) {
         // TTL constants
         const ttlWorkSchool = 6;
@@ -215,7 +172,6 @@ export class NotificationService {
     getTimeNum(timeStr: string) {
         var timeArr = timeStr.split(":");
         var timeNum = (parseInt(timeArr[0]) * 60) + parseInt(timeArr[1]);
-        // console.log("Converted " + timeStr + " to " + timeNum);
 
         return timeNum;
     }
